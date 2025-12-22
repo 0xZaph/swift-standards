9ef208c0374ffb1a453d1fdca0ecfff186abe730
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-12-22 18:52:24 +0100

    Add Layout.Grid.Lazy for responsive grid layouts
    - Add Layout.Grid.Lazy with fractional column sizing (CSS fr units)
    - Column modes: .count(n), .fractions([...]), .autoFill/autoFit(minWidth:)
    - Functorial map for content transformation
    - Add Snapshot suite to #TestSuites macro

--- Sources/StandardsTestSupportMacros/TestSuitesMacro.swift
+++ Sources/StandardsTestSupportMacros/TestSuitesMacro.swift
@@ -25,6 +25,7 @@ public struct TestSuitesMacro: DeclarationMacro {
                 @Suite(.exclusive(group: \(literal: typeGroup))) struct Unit {}
                 @Suite(.exclusive(group: \(literal: typeGroup))) struct EdgeCase {}
                 @Suite(.exclusive, .serialized) struct Performance {}
+                @MainActor @Suite(.serialized) struct Snapshot {}
             }
             """
         ]
